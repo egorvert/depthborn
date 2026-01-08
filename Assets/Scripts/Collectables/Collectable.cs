29bc784c314d67933bf22cb252616909c923f5fb
PSYEONE <154057250+PSYEONE@users.noreply.github.com>
2026-01-08 22:00:09 +0000

    Revert "3d sound with added settings controls"

--- Assets/Scripts/Collectables/Collectable.cs
+++ Assets/Scripts/Collectables/Collectable.cs
@@ -4,47 +4,12 @@ using UnityEngine;
 
 public class Collectable : MonoBehaviour
 {
-	[Header("Audio Settings")]
-    [SerializeField] private float fadeDuration = 1.0f;
-	private AudioSource audioSource;
-	
-	private void Awake()
-    {
-        audioSource = GetComponent<AudioSource>();
-    }
-	
     private void OnTriggerEnter(Collider other){
 		PlayerInventory playerInventory = other.GetComponent<PlayerInventory>();
 		
 		if (playerInventory != null){
 			playerInventory.IncrementCollected();
-			
-			StartCoroutine(FadeOutAndDisable());
+			gameObject.SetActive(false);
 		}
 	}
-	
-	private IEnumerator FadeOutAndDisable()
-    {
-        // Disable collider
-        GetComponent<Collider>().enabled = false;
-
-        // Disable the Visuals so it looks "collected"
-        Renderer rend = GetComponentInChildren<Renderer>();
-        if (rend != null) rend.enabled = false;
-
-        // Fade volume
-        float startVolume = audioSource.volume;
-        float currentTime = 0;
-
-        while (currentTime < fadeDuration)
-        {
-            currentTime += Time.deltaTime;
-            audioSource.volume = Mathf.Lerp(startVolume, 0f, currentTime / fadeDuration);
-            yield return null;
-        }
-		
-        audioSource.Stop();
-        audioSource.volume = startVolume;
-        gameObject.SetActive(false);
-    }
 }
